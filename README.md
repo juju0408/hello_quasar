40a48d1adb579786139a7f98950350494b97e22c
juju0408 <75842553+juju0408@users.noreply.github.com>
2020-12-31 13:52:53 +0900

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# hello_quasar
+# Quasar App (hello_quasar)
+
+A Quasar Framework app
+
+## 필요한 모듈 설치
+```bash
+yarn
+```
+
+### 개발 실행
+```bash
+quasar dev
+```
+
+
+### 릴리즈 빌드
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-gutter-lg " style="max-width: 300px">
+      <q-btn icon="mail" padding="lg" color="primary" text-color="white" label="TypePage" @click="gotoPage('/type')"/>
+      <q-btn icon="mail" padding="lg" color="primary" text-color="white" label="TestPage" @click="gotoPage('/test')"/>
+      <!-- <q-input v-model="text" label="standard" outlined mask="NNN-NNN-NNNN"/>
+      <q-radio v-model="shape" val="line" label="선" />
+      <q-radio v-model="shape" val="rectangle" label="사각형" />
+      <q-radio v-model="shape" val="ellipse" label="타원" />
+      <q-radio v-model="shape" val="polygon" label="다각형" />
+      <q-checkbox v-model="right" label="면 추가" /> -->
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      text : 'ym',
+      shape: 'line',
+      right : false,
+    }
+  },
+  methods : {
+    gotoPage(url) {
+      this.$router.push(url);
+    }
+  }
+}
+</script>
